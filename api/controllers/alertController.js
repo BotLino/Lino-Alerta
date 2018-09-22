8d84e0e9e53ba9f0f1dbc2342cb4b063447a42ef
Icaro Oliveira <icarooliv2@gmail.com>
2018-09-22 00:26:19 -0300

    [Issue_83] Adds folder structure, server connection and database
    Co-authored-by: Icaro Oliveira <icarooliv2@gmail.com>
    Co-authored-by: Gabriel Filipe <gabrielfilipe7unb@gmail.com>

--- api/controllers/alertController.js
+++ api/controllers/alertController.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
 global.atob = require("atob");
-var htmlToText = require('html-to-text');
+var htmlToText = require("html-to-text");
 
 /*
 * Estamos pegando apenas a utima mensagem. Porem, ele só é disparado uma vez.
@@ -15,14 +15,13 @@ var htmlToText = require('html-to-text');
 // - chegou uma msg!! toma aqui os ids!!
 // - vou pegar  o id e pesquisar no meu metodo getMessage para saber qual é a mensagem
 
-
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-const TOKEN_PATH = 'token.json';
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+const TOKEN_PATH = "./api/resources/token.json";
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
+fs.readFile("./api/resources/credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
   // Authorize a client with credentials, then call the Gmail API.
   authorize(JSON.parse(content), getRecentEmail);
 });
@@ -34,9 +33,12 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
@@ -54,23 +56,23 @@ function authorize(credentials, callback) {
  */
 function getNewToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
+    access_type: "offline",
+    scope: SCOPES
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
+  console.log("Authorize this app by visiting this url:", authUrl);
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
+    output: process.stdout
   });
-  rl.question('Enter the code from that page here: ', (code) => {
+  rl.question("Enter the code from that page here: ", code => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
+      if (err) return console.error("Error retrieving access token", err);
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
         if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
+        console.log("Token stored to", TOKEN_PATH);
       });
       callback(oAuth2Client);
     });
@@ -83,27 +85,30 @@ function getNewToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listLabels(auth) {
-  const gmail = google.gmail({version: 'v1', auth});
-  gmail.users.labels.list({
-    userId: 'me',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const labels = res.data.labels;
-    if (labels.length) {
-      console.log('Labels:');
-      labels.forEach((label) => {
-        console.log(`- ${label.name}`);
-      });
-    } else {
-      console.log('No labels found.');
+  const gmail = google.gmail({ version: "v1", auth });
+  gmail.users.labels.list(
+    {
+      userId: "me"
+    },
+    (err, res) => {
+      if (err) return console.log("The API returned an error: " + err);
+      const labels = res.data.labels;
+      if (labels.length) {
+        console.log("Labels:");
+        labels.forEach(label => {
+          console.log(`- ${label.name}`);
+        });
+      } else {
+        console.log("No labels found.");
+      }
     }
-  });
+  );
 }
 /**
-   * Get the recent email from your Gmail account
-   *
-   * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-   */
+ * Get the recent email from your Gmail account
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
 
 function getRecentEmail(auth) {
   // Only get the recent email - 'maxResults' parameter
@@ -129,7 +134,6 @@ function getRecentEmail(auth) {
           }
           // console.log(response.data.payload);
 
-
           var parts = [response.data.payload];
 
           while (parts.length) {
@@ -138,34 +142,35 @@ function getRecentEmail(auth) {
               parts = parts.concat(part.parts);
             }
 
-            if(part.mimeType === 'text/html') {
-              var decodedPart = decodeURIComponent(escape(atob(part.body.data.replace(/\-/g, '+').replace(/\_/g, '/'))));
-              
+            if (part.mimeType === "text/html") {
+              var decodedPart = decodeURIComponent(
+                escape(
+                  atob(part.body.data.replace(/\-/g, "+").replace(/\_/g, "/"))
+                )
+              );
+
               var text = htmlToText.fromString(decodedPart, {
                 wordwrap: 130
               });
               console.log(text);
-              
-            
+
               // console.log(decodedPart);
-            
             }
-          }          
+          }
 
           // var extractField = function(json, fieldName) {
           //   return response["data"].payload.headers.filter(function(header) {
-              
+
           //     return header.name === fieldName;
           //   })[0].value;
           // };
           // var date = extractField(response, "Date");
           // var from = extractField(response, "From");
           // var subject = extractField(response, "Subject");
-        
+
           // console.log(date);
           // console.log(from);
           // console.log(subject);
-         
         }
       );
     }
