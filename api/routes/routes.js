dcb398908e9f2c260c5f54b3136413aac5143389
Luiz Guilherme Silva <do.guilherme@hotmail.com>
2019-06-13 13:22:21 -0300

    Fix routes according to ES6

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,44 +1,46 @@
-"use strict";
-module.exports = function(app) {
-  var alert = require("../controllers/alertController");
-  var NotifierModel = require("../models/notifierModel");
-  var AlertModel = require("../models/alertModel");
-  var NotifierController = require("../controllers/notifierController");
 
-  app.get("/newAlert", async (req, res, next) => {
+module.exports = function routes(app) {
+  const alert = require('../controllers/alertController');
+  const NotifierModel = require('../models/notifierModel');
+  const AlertModel = require('../models/alertModel');
+  const NotifierController = require('../controllers/notifierController');
+
+  app.get('/newAlert', async (req, res, next) => {
     try {
-      var newAlert = new AlertModel();
+      let newAlert = new AlertModel();
       newAlert = await alert.callGetRecentEmailId();
-      var result = "";
-      var name = "";
+      let result = '';
+      let name = '';
       await NotifierModel.findOne({
-        email: newAlert.email
+        email: newAlert.email,
       })
-        .then(doc => {
+        .then((doc) => {
           result = doc;
         })
-        .catch(err => {
+        .catch((err) => {
           res.status(500).json(err);
         });
       try {
         if (result) {
-          name = result.name;
+          name = { result: name };
           newAlert.name = name;
           await newAlert
             .save()
-            .then(async doc => {
+            .then(async (doc) => {
               if (!doc || doc.length === 0) {
                 return res.status(500).send(doc);
               }
-              await console.log("New Message sent: \n", newAlert);
+              await console.log('New Message sent: \n', newAlert);
               await res.json(newAlert);
               res.status(200).send(doc);
+              return (console.log(res.status(200).send(doc)));
             })
-            .catch(err => {
-              res.status(404).json("No new messages found");
+            .catch((err) => {
+              res.status(404).json('No new messages found');
+              return (console.log(err));
             });
         } else {
-          res.status(401).json("Forbidden");
+          res.status(401).json('Forbidden');
         }
       } catch (e) {
         console.log(e);
@@ -48,36 +50,39 @@ module.exports = function(app) {
     }
   });
 
-  app.post("/newUser", (req, res) => {
+  app.post('/newUser', (req, res) => {
     if (!req.body) {
-      return res.status(400).send("Request body is missing");
+      return res.status(400).send('Request body is missing');
     }
 
     if (!req.body.email) {
-      return res.status(400).send("Request body is missing");
+      return res.status(400).send('Request body is missing');
     }
 
     console.log(req.body);
 
-    var model = new NotifierModel(req.body);
+    const model = new NotifierModel(req.body);
     model
       .save()
-      .then(doc => {
+      .then((doc) => {
         if (!doc || doc.length === 0) {
           return res.status(500).send(doc);
         }
         res.status(201).send(doc);
+        return (console.log('error 201'));
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json(err);
       });
+
+    return (console.log(res));
   });
 
-  app.get("/getUser", (req, res) => {
+  app.get('/getUser', (req, res) => {
     if (!req.query.email) {
-      return res.status(400).send("Missing URL parameter: email");
-    } else {
-      NotifierController.getUserByEmail(req.query.email, res);
+      return res.status(400).send('Missing URL parameter: email');
     }
+    NotifierController.getUserByEmail(req.query.email, res);
+    return (console.log(res));
   });
 };
